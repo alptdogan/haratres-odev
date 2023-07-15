1da830b74765d1a425a4f68583a23ef10021b979
alptdogan <alpdogan000@gmail.com>
2023-07-15 19:37:08 +0300

    Add scanner and try-catch blocks with if conditions that throw exception

--- src/AlisverisSepeti.java
+++ src/AlisverisSepeti.java
@@ -24,67 +24,104 @@
 //        Kod yazım kurallarına uygun ise 2 puan.
 
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class AlisverisSepeti {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         // ChatGpt yardımıyla bir sonuca varmaya çalıştım, kendisi doğrudan sonuç vermedi kendi kodumun üstüne biraz yardımla stream ile tamamlama yaptım.
         // Array'e eleman ekleme işlemini de açıkçası ondan öğrendim. Bunu bir önceki soruda kullanabilirim.
         // try-catch'ler için geri geleceğim
 
-        int[] A = {5, 6, 4, 3, 4};
-        int[] B = new int[A.length];
-        int[] C = new int[A.length];
-        int totalAmount = 0;
-
+//        int[] A = {5, 6, 4, 3, 4};
         /*{ 5, 2, 1, 3, 4 }*/
 
-        for (int i = 0; i < A.length; i++) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Gireceğiniz array'in kaç değer alacağını belirtin: ");
+        int arrLength = sc.nextInt();
 
-            if (i == 0 && A[i] < A[i + 1]) {
-                B[i] = A[i];
-            } else if (i > 0 && i < A.length - 1 && A[i] < A[i + 1]) {
-                B[i] = A[i];
-            } else if (i < A.length - 1){
-                B[i] = A[i] - A[i + 1];
-            } else if (i == A.length - 1 && A[i] >= A[i - 1]) {
-                B[i] = A[i];
-            } else if (i == A.length && A[i] < A[i - 1]) {
-                B[i] = A[i - 1] - A[i];
+
+        try {
+            if (1 < arrLength && arrLength < 100) {
+
+                int[] A = new int[arrLength];
+
+                try {
+                for (int i = 0; i < arrLength; i++) {
+
+                    System.out.println("Array'deki " + (i + 1) + ". sayıyı giriniz: ");
+                    A[i] = sc.nextInt();
+
+                    if (A[i] <= 0 && 2000 <= A[i]) {
+                        throw new Exception();
+                    }
+
+                }
+                } catch (Exception exc) {
+                    System.out.println("Array elemanları 0 ile 2000 arasında olmalıdır.");
+                }
+
+                int[] B = new int[A.length];
+                int[] C = new int[A.length];
+                int totalAmount = 0;
+
+
+                    for (int i = 0; i < A.length; i++) {
+
+//                        if (A[i] <= 0 && 2000 <= A[i]) {
+//                            throw new Exception();
+//                        }
+
+                        if (i == 0 && A[i] < A[i + 1]) {
+                            B[i] = A[i];
+                        } else if (i > 0 && i < A.length - 1 && A[i] < A[i + 1]) {
+                            B[i] = A[i];
+                        } else if (i < A.length - 1) {
+                            B[i] = A[i] - A[i + 1];
+                        } else if (i == A.length - 1 && A[i] >= A[i - 1]) {
+                            B[i] = A[i];
+                        } else if (i == A.length && A[i] < A[i - 1]) {
+                            B[i] = A[i - 1] - A[i];
+                        }
+                    }
+
+                B[B.length - 1] = A[A.length - 1];
+
+                for (int i = 0; i < B.length; i++) {
+                    totalAmount += B[i];
+                }
+
+                int j = 0;
+                for (int i = 0; i < A.length; i++) {
+
+                    if (A[i] == B[i] && A[i] != 0) {
+                        C[j] = A[i];
+                        j++;
+                    }
+                }
+
+                Arrays.sort(C);
+
+                int[] D;
+
+                D = IntStream.range(0, C.length)
+                        .filter(n -> C[n] != 0)
+                        .map(n -> C[n])
+                        .toArray();
+
+
+                System.out.println("İndirim sonucunda bütün ürünler: " + Arrays.toString(B));
+                System.out.println("Toplam: " + totalAmount + " Lira");
+                System.out.println("İndirimsiz olanlar: " + Arrays.toString(D));
+
+            } else {
+                throw new Exception();
             }
+        }catch (Exception exc) {
+            System.out.println("Arrayinizin boyu 1 ile 100 arasında olmalıdır.");
         }
-
-        B[B.length - 1] = A[A.length - 1];
-
-        for (int i = 0; i < B.length; i++) {
-            totalAmount += B[i];
-        }
-
-        int j = 0;
-        for (int i = 0; i  < A.length; i++) {
-
-            if (A[i] == B[i] && A[i] != 0) {
-                C[j] = A[i];
-                j++;
-            }
-        }
-
-        Arrays.sort(C);
-
-        int[] D;
-
-        D = IntStream.range(0, C.length)
-                .filter(n -> C[n] != 0)
-                .map(n -> C[n])
-                .toArray();
-
-
-        System.out.println("İndirim sonucunda bütün ürünler: " + Arrays.toString(B));
-        System.out.println("Toplam: " + totalAmount + " Lira (Birim)");
-        System.out.println("İndirimsiz olanlar: " + Arrays.toString(D));
-
     }
 
 }
