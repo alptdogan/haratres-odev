ab43a3e045da1483ad6d2fef6f18b96ac292476d
alptdogan <alpdogan000@gmail.com>
2023-07-15 19:43:20 +0300

    Refactor for the second catch

--- src/AlisverisSepeti.java
+++ src/AlisverisSepeti.java
@@ -14,14 +14,10 @@
 //
 //        Kısıtlar: Arrayimizin boyu  1 < n < 100    arrayimizin elemanları ise 0 < i < 2.000
 //
-//        Ödev java programlama dilinde yazılacaktır. Her ödev ayrı sınıfta olacaktır. Değerlendirme işlemi her için önceden yazılmış unit test methodlarından geçirilerek yapılacaktır.
+//        Ödev java programlama dilinde yazılacaktır. Her ödev ayrı sınıfta olacaktır.
+//        Değerlendirme işlemi her için önceden yazılmış unit test methodlarından geçirilerek yapılacaktır.
 //
 //        Kısıtlara uymayan durumlarda system exception throw etmeli.
-//
-//
-//        Çıktı doğru ise 5 puan .
-//        Kısıtlara uyulmuş ise 3 puan.
-//        Kod yazım kurallarına uygun ise 2 puan.
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -33,7 +29,6 @@ public class AlisverisSepeti {
 
         // ChatGpt yardımıyla bir sonuca varmaya çalıştım, kendisi doğrudan sonuç vermedi kendi kodumun üstüne biraz yardımla stream ile tamamlama yaptım.
         // Array'e eleman ekleme işlemini de açıkçası ondan öğrendim. Bunu bir önceki soruda kullanabilirim.
-        // try-catch'ler için geri geleceğim
 
 //        int[] A = {5, 6, 4, 3, 4};
         /*{ 5, 2, 1, 3, 4 }*/
@@ -48,19 +43,15 @@ public class AlisverisSepeti {
 
                 int[] A = new int[arrLength];
 
-                try {
                 for (int i = 0; i < arrLength; i++) {
 
                     System.out.println("Array'deki " + (i + 1) + ". sayıyı giriniz: ");
                     A[i] = sc.nextInt();
 
-                    if (A[i] <= 0 && 2000 <= A[i]) {
-                        throw new Exception();
+                    if (A[i] <= 0 || 2000 <= A[i]) {
+                        throw new IndexOutOfBoundsException();
                     }
 
-                }
-                } catch (Exception exc) {
-                    System.out.println("Array elemanları 0 ile 2000 arasında olmalıdır.");
                 }
 
                 int[] B = new int[A.length];
@@ -69,10 +60,6 @@ public class AlisverisSepeti {
 
 
                     for (int i = 0; i < A.length; i++) {
-
-//                        if (A[i] <= 0 && 2000 <= A[i]) {
-//                            throw new Exception();
-//                        }
 
                         if (i == 0 && A[i] < A[i + 1]) {
                             B[i] = A[i];
@@ -85,6 +72,7 @@ public class AlisverisSepeti {
                         } else if (i == A.length && A[i] < A[i - 1]) {
                             B[i] = A[i - 1] - A[i];
                         }
+
                     }
 
                 B[B.length - 1] = A[A.length - 1];
@@ -119,6 +107,8 @@ public class AlisverisSepeti {
             } else {
                 throw new Exception();
             }
+        }catch (IndexOutOfBoundsException e) {
+            System.out.println("Array elemanları 0 ile 2000 arasında olmalıdır.");
         }catch (Exception exc) {
             System.out.println("Arrayinizin boyu 1 ile 100 arasında olmalıdır.");
         }
